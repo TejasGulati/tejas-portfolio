a46c676bba305dba734b1c0e337e268192952548
Tejas <tejasgulati11227@gmail.com>
2025-05-01 22:57:36 +0530

    Push

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.VITE_BUILD_PATH || "/portfolio/", // Uses env var or defaults to /portfolio/ for GitHub Pages
+  base: process.env.VITE_BASE_PATH || "./",
   plugins: [react()],
 });
